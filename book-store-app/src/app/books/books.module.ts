a9e9f4d72ca3451d27414a9ab0986bf8cf1425aa
dev-JoseAlberto <jsalberto2016@outlook.com>
2022-04-29 11:36:57 -0300

    I'm improving layout of book store app.

--- book-store-app/src/app/books/books.module.ts
+++ book-store-app/src/app/books/books.module.ts
@@ -3,19 +3,31 @@ import { CommonModule } from '@angular/common';
 
 import { BooksComponent } from './books.component';
 import { BooksListComponent } from './books-list/books-list.component';
+import { HeaderComponent } from './html-elements/header/header.component';
+import { NavComponent } from './html-elements/nav/nav.component';
+import { FooterComponent } from './html-elements/footer/footer.component';
+import { SlideShowComponent } from './html-elements/slide-show/slide-show.component';
 
 
 
 @NgModule({
   declarations: [
     BooksComponent,
-    BooksListComponent
+    BooksListComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    SlideShowComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    BooksComponent
+    BooksComponent,
+    HeaderComponent,
+    NavComponent,
+    SlideShowComponent,
+    FooterComponent
   ]
 })
 export class BooksModule { }
